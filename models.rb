d75ae17998ef8da1437f25bc54194b53e243d91d
Shahid <shahid.sarker@gmail.com>
2020-01-06 12:45:00 -0500

    Models

--- models.rb
+++ models.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
+  # validates is an ActiveRecord function
+  # symbols are treated as column names
+  # runs these constraints before the .save function
   validates :email, :password, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 5, maximum: 8 }
 end
 
 class Dog < ActiveRecord::Base
